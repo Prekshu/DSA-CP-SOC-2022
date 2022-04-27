ebbaa563af4c4c1f56bcc9b674e664503a283a6d
Prekshu <104462716+Prekshu@users.noreply.github.com>
2022-04-28 03:34:08 +0530

    Add files via upload

---
+++ CSES_TowerOfHanoi.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+void hanoi(int n,int a,int b,int c)
+{
+    if(n==1)
+    {
+        cout<<a<<" "<<c<<endl;
+        return;
+    }
+    hanoi(n-1,a,c,b);
+    cout<<a<<" "<<c<<endl;
+    hanoi(n-1,b,a,c);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<(int)pow(2,n)-1<<endl;
+    hanoi(n,1,2,3);
+    return 0;
+}
